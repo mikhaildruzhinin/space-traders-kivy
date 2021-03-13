52aea32459d362c042909cddbf391e811cdc9e7f
Mikhail Druzhinin <druzhininmy@gmail.com>
2021-03-14 01:20:26 +0300

    add ships, market, map and repay loan

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class SpaceTraders(object):
     def status(self):
         r = requests.get(f'{self.base_url}/game/status')
         r.raise_for_status()
-        return r.json()
+        data = r.json()
+        return data
 
     def generate_token(self):
         try:
@@ -37,7 +38,85 @@ class SpaceTraders(object):
     
     def take_loan(self):
         params = {'token': self.token}
-        body = {'type': 'STARTUP'}
-        r = requests.post(f'{self.base_url}/users/{self.username}/loans', params=params, json=body)
+        payload = {'type': 'STARTUP'}
+        r = requests.post(f'{self.base_url}/users/{self.username}/loans', params=params, json=payload)
+        data = r.json()
+        return data
+
+    def get_ships(self, class_=None):
+        params = {
+            'token': self.token,
+            'class': class_,
+        }
+        r = requests.get(f'{self.base_url}/game/ships', params=params)
+        data = r.json()
+        return data
+
+    def buy_ship(self, location, type_):
+        params = {'token': self.token}
+        payload = {
+            'location': location,
+            'type': type_,
+        }
+        r = requests.post(f'{self.base_url}/users/{self.username}/ships', params=params, json=payload)
+        data = r.json()
+        return data
+
+    def buy_goods(self, ship_id, good, quantity):
+        params = {'token': self.token}
+        payload = {
+            'shipId': ship_id,
+            'good': good,
+            'quantity': quantity,
+        }
+        r = requests.post(f'{self.base_url}/users/{self.username}/purchase-orders', params=params, json=payload)
+        data = r.json()
+        return data
+
+    def get_market(self, location):
+        params = {'token': self.token}
+        r = requests.get(f'{self.base_url}/game/locations/{location}/marketplace', params=params)
+        data = r.json()
+        return data
+
+    def get_system_map(self, system, type_=None):
+        params = {
+            'token': self.token,
+            'type': type_,
+        }
+        r = requests.get(f'{self.base_url}/game/systems/{system}/locations', params=params)
+        data = r.json()
+        return data
+
+    def create_flight_plan(self, ship_id, destination):
+        params = {'token': self.token}
+        payload = {
+            'shipId': ship_id,
+            'destination': destination,
+        }
+        r = requests.post(f'{self.base_url}/users/{self.username}/flight-plans', params=params, json=payload)
+        data = r.json()
+        return data
+
+    def get_flight_plan(self, flight_plan_id):
+        params = {'token': self.token}
+        r = requests.get(f'{self.base_url}/users/{self.username}/flight-plans/{flight_plan_id}', params=params)
+        data = r.json()
+        return data
+
+    def sell_goods(self, ship_id, good, quantity):
+        params = {'token': self.token}
+        payload = {
+            'shipId': ship_id,
+            'good': good,
+            'quantity': quantity,
+        }
+        r = requests.post(f'{self.base_url}/users/{self.username}/sell-orders', params=params, json=payload)
+        data = r.json()
+        return data
+
+    def repay_loan(self, loan_id):
+        params = {'token': self.token}
+        r = requests.put(f'{self.base_url}/users/{self.username}/loans/{loan_id}', params=params)
         data = r.json()
         return data
